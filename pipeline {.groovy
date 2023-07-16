51c11745ce5bc6087c58a04b60bc8d4eec6e5316
Lyriq Sele <lyriqsele@Lyriqs-MacBook-Pro.local>
2023-07-16 11:16:04 -0400

    v2

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -1,33 +1,39 @@
 pipeline {
   agent any
-    tools {
-      maven 'maven3'
-                 jdk 'JDK8'
+
+  tools {
+    maven 'maven3'
+    jdk 'JDK8'
+  }
+
+  stages {
+    stage('Checkout') {
+      steps {
+        checkout scm
+      }
     }
-    stages {      
-        stage('Build maven ') {
-            steps { 
-                    sh 'pwd'      
-                    sh 'mvn  clean install package'
-            }
-        }
-        
-        stage('Copy Artifact') {
-           steps { 
-                   sh 'pwd'
-		   sh 'cp -r target/*.jar docker'
-           }
-        }
-         
-        stage('Build docker image') {
-           steps {
-               script {         
-                 def customImage = docker.build('initsixcloud/petclinic', "./docker")
-                 docker.withRegistry('https://hub.docker.com/repository/docker/lyriqsele/dockerhub/general', 'dockerhub') {
-                 customImage.push("${env.BUILD_NUMBER}")
-                 }                     
-           }
-        }
-	  }
+
+    stage('Build maven') {
+      steps {
+        sh 'mvn clean install package'
+      }
     }
+
+    stage('Copy Artifact') {
+      steps {
+        sh 'cp -r target/*.jar docker'
+      }
+    }
+
+    stage('Build docker image') {
+      steps {
+        script {
+          def customImage = docker.build('initsixcloud/petclinic', "./docker")
+          docker.withRegistry('https://hub.docker.com/repository/docker/lyriqsele/dockerhub/general', 'dockerhub') {
+            customImage.push("${env.BUILD_NUMBER}")
+          }
+        }
+      }
+    }
+  }
 }
